c7a6173d1fb486ad7b096461c480be102152b545
Konstantinos Koliopoulos <konst.koliop@gmail.com>
2024-08-14 02:03:07 +0300

    Add minifier plugins, use dist directory

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import { geoJSON, map, tileLayer, GeoJSON, LatLng, control } from "leaflet";
+import {
+  geoJSON,
+  map,
+  tileLayer,
+  GeoJSON,
+  LatLng,
+  control,
+  tooltip,
+  Point,
+} from "leaflet";
 
-import koin from "./dimkoin.json";
-import dim from "./dim.json";
-
-import dimdata from "./dimdata.json";
-import koindata from "./koindata.json";
-
-function coordsToLatLngDim(coords) {
+function coordsToLatLngCustom(coords) {
   return new LatLng(coords[1] + 0.00255, coords[0] + 0.0019, coords[2]);
 }
 
@@ -30,7 +33,33 @@ let overlayMaps;
 let dimLayer;
 let koinLayer;
 
-document.addEventListener("DOMContentLoaded", () => {
+async function initialize() {
+  const getKoin = async () => {
+    const r = await fetch("./dimkoin.json");
+    return await r.json();
+  };
+  const getDim = async () => {
+    const r = await fetch("./dim.json");
+    return await r.json();
+  };
+  const getDimData = async () => {
+    const r = await fetch("./dimdata.json");
+    return await r.json();
+  };
+  const getKoinData = async () => {
+    const r = await fetch("./koindata.json");
+    return await r.json();
+  };
+
+  const getGeoDataPromise = Promise.all([getKoin(), getDim()]);
+  let dimdata, koindata;
+  try {
+    [dimdata, koindata] = await Promise.all([getDimData(), getKoinData()]);
+  } catch (e) {
+    alert("Oops! Something went wrong...1 " + e);
+    return;
+  }
+
   dimdata.forEach((d) => {
     dimPopMap.set(d["Γεωγραφικός κωδικός"], d);
   });
@@ -63,35 +92,82 @@ document.addEventListener("DOMContentLoaded", () => {
   //     );
   //   }
   // });
+
+  let koin, dim;
+  try {
+    [koin, dim] = await getGeoDataPromise;
+  } catch (e) {
+    alert("Oops! Something went wrong... " + e);
+    return;
+  }
+
   dimLayer = geoJSON(dim, {
-    coordsToLatLng: coordsToLatLngDim,
-    style: (feature) => {
+    coordsToLatLng: coordsToLatLngCustom,
+    style: () => {
       return { color: "#db3609" };
     },
-  }).bindPopup((layer) => {
-    console.log(layer);
-    const d = dimPopMap.get(layer.feature.properties.CODE);
-    return `<h2>${layer.feature.properties.NAME_GR}</h2><p class="tooltip-pop">Πληθυσμός 2021: <b>${d["Μόνιμος Πληθυσμός 2021"]}</b></p>` /* + d["Περιγραφή"] */;
-  });
+    onEachFeature: (_feature, layer) => {
+      layer.on("mouseover", () => {
+        layer.setStyle({ color: "#455d75" });
+      });
+      layer.on("mouseout", () => {
+        layer.setStyle({ color: "#db3609" });
+      });
+    },
+  }).bindTooltip(
+    (layer) => {
+      // console.log(layer);
+      const d = dimPopMap.get(layer.feature.properties.CODE);
+      return `<h2>${layer.feature.properties.NAME_GR}</h2><p class="tooltip-pop">Πληθυσμός 2021: <b>${d["Μόνιμος Πληθυσμός 2021"]}</b></p>` /* + d["Περιγραφή"] */;
+    },
+    { sticky: true, offset: [50, 0] }
+  );
 
   koinLayer = geoJSON(koin, {
-    coordsToLatLng: coordsToLatLngDim,
-    style: (feature) => {
+    coordsToLatLng: coordsToLatLngCustom,
+    style: () => {
       return { color: "#334455" };
     },
-  }).bindPopup((layer) => {
-    console.log(layer);
-    const d = koinPopMap.get(layer.feature.properties.KAL2022);
-    return `<h2>${layer.feature.properties.LAU_LABEL3}</h2><p class="tooltip-pop">Πληθυσμός 2021: <b>${d["ΜΟΝΙΜΟΣ ΠΛΗΘΥΣΜΟΣ"]}</b></p>` /* + d["Περιγραφή"] */;
-  });
+    onEachFeature: (_feature, layer) => {
+      layer.on("mouseover", () => {
+        // layer.setStyle({ color: "#8ebbe8" });
+        layer.setStyle({ color: "#8a2307" });
+      });
+      layer.on("mouseout", () => {
+        layer.setStyle({ color: "#334455" });
+      });
+    },
+  }).bindTooltip(
+    (layer) => {
+      // console.log(layer);
+      const code = layer.feature.properties.KAL2022;
+      const d = koinPopMap.get(code);
+      return `<h2>${layer.feature.properties.LAU_LABEL3}</h2><h3>${
+        koinPopMap.get(code.slice(0, 4)).ΠΕΡΙΓΡΑΦΗ
+      }</h3><p class="tooltip-pop">Πληθυσμός 2021: <b>${
+        d["ΜΟΝΙΜΟΣ ΠΛΗΘΥΣΜΟΣ"]
+      }</b></p>` /* + d["Περιγραφή"] */;
+    },
+    { sticky: true, offset: [50, 0] }
+  );
 
   overlayMaps = { Δήμοι: dimLayer, "Δημοτικές Κοινότητες": koinLayer };
 
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", createMap);
+  } else {
+    createMap();
+  }
+}
+
+function createMap() {
   theMap = map("map", {
     zoom: 7,
     center: [38.5253, 22.3753],
-    preferCanvas: true,
-    layers: [mapLayer, dimLayer, koinLayer],
+    // preferCanvas: true,
+    layers: [mapLayer, dimLayer],
   });
   control.layers(baseMaps, overlayMaps).addTo(theMap);
-});
+}
+
+initialize();
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,63 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const JsonMinimizerPlugin = require("json-minimizer-webpack-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.json$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: { chunks: "all", minChunks: 2 },
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      `...`,
+      new JsonMinimizerPlugin(),
+      new HtmlMinimizerPlugin(),
+      new CssMinimizerPlugin(),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          to: path.resolve(__dirname, "dist"),
+          context: "./src",
+          from: "*.css",
+        },
+        {
+          to: path.resolve(__dirname, "dist"),
+          context: "./src",
+          from: "*.html",
+        },
+        {
+          to: path.resolve(__dirname, "dist"),
+          context: "./src",
+          from: "*.json",
+        },
+      ],
+    }),
+  ],
   output: {
     filename: "index.js",
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "dist"),
   },
-};
+} /* as import("webpack").Configuration */;
